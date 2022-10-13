3ae813c128e523996b47896fee0d84e000bff668
Ivan Zabolotniy <mustadonte@gmail.com>
2022-10-13 11:10:49 +0300

    fix JOI messages

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -6,9 +6,24 @@ const Joi = require("joi");
 
 const schema = Joi.object({
   id: Joi.string(),
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  phone: Joi.string().min(5).required(),
+  name: Joi.string().required().messages({
+    "string.base": `"" should be a type of string`,
+    "string.empty": `"" must contain value`,
+    "string.pattern.base": `"" must be 10 digit number`,
+    "any.required": "missing required name field",
+  }),
+  email: Joi.string().email().required().messages({
+    "string.base": `"" should be a type of string`,
+    "string.empty": `"" must contain value`,
+    "string.pattern.base": `"" must be 10 digit number`,
+    "any.required": "missing required email field",
+  }),
+  phone: Joi.string().min(7).required().messages({
+    "string.base": `"" should be a type of string`,
+    "string.empty": `"" must contain value`,
+    "string.pattern.base": `"" must be 7 digit number`,
+    "any.required": "missing required phone field",
+  }),
 });
 
 router.get("/", async (req, res, next) => {
