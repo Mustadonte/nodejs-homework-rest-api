44bcdce756dede9594f61f3535f7a95b67dfdbea
Ivan Zabolotniy <mustadonte@gmail.com>
2022-10-18 10:03:49 +0300

    creating connect to heroku

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Mustadonte:aqWzTcWQmiqHUFdX@cluster0.uptsfxc.mongodb.net/db_contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .catch((error) => console.log(error.message));
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
