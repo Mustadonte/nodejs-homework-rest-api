248e81152ce3f8c3c77b5ccca6bcf44c60ff39bd
Ivan Zabolotniy <mustadonte@gmail.com>
2022-10-27 21:16:50 +0300

    fix favorite filter

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,11 +1,11 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const { page = 1, limit = 20, favorite = true } = req.query;
+  const { page = 1, limit = 20, ...filter } = req.query;
   const skip = (page - 1) * limit;
 
   const result = await Contact.find(
-    { owner: req.user.id, favorite },
+    { owner: req.user.id, ...filter },
     "-createdAt -updatedAt",
     {
       skip,
